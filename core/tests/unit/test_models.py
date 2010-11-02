07d9fdf8a5864b69b78ff4c0f55123ef5932a251
Francisco Souza <francisco@franciscosouza.net>
2010-11-02 00:33:42 -0200

    Refactoring models tests

--- core/tests/unit/test_models.py
+++ core/tests/unit/test_models.py
@@ -5,17 +5,20 @@ from nose.tools import raises
 
 class TestModels(mocker.MockerTestCase):
 
+    def _get_conference(self, title = None, description = None):
+        conference = Conference(title = title, description = description)
+        return conference
+
     @raises(IntegrityError)
     def should_not_add_a_conference_without_title(self):
-        c = Conference()
-        c.title = None
-        c.description = 'My description'
+        c = self._get_conference(title=None, description='My description')
         c.save()
 
     @raises(IntegrityError)
     def should_not_add_a_conference_without_description(self):
-        c = Conference()
-        c.title = 'My title'
-        c.description = None
+        c = self._get_conference(title='My title', description=None)
         c.save()
 
+    def should_add_a_conference_with_title_and_description(self):
+        c = self._get_conference(title='My title', description='My description')
+        c.save()
