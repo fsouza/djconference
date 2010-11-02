1b6a8ecf2d7c99fd80a7c91479123509b0a069b4
Francisco Souza <francisco@franciscosouza.net>
2010-11-02 01:51:43 -0200

    Making the 5th step green :D

--- core/views.py
+++ core/views.py
@@ -2,9 +2,16 @@
 from core.forms import ConferenceForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def new_conference(request):
     form = ConferenceForm()
+    if request.method == 'POST':
+        form = ConferenceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('list_conferences'))
     return render_to_response('new_conference.html', {
         'form' : form
         }, context_instance=RequestContext(request)
