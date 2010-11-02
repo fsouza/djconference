4ac0b5ed1e6dd62fd5797e782ad7d784c94c5ac6
Francisco Souza <francisco@franciscosouza.net>
2010-11-02 01:23:28 -0200

    Fixed functional test

--- core/tests/functional/test_views.py
+++ core/tests/functional/test_views.py
@@ -3,6 +3,7 @@ from nose.tools import assert_true
 
 class FunctionalConferenceViewsSpec(FunctionalCoreTestCase):
 
-    def test_something(self):
+    def test_new_conference_url_with_text(self):
+        "New conference url (/conferences/new) should contain the text \"New conference\""
         response = self.client.get('/conferences/new')
         assert_true('New conference' in response.content)
