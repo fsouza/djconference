b5d4f964e83fad4e87eec8499f6919d66dfde2e1
Francisco Souza <francisco@franciscosouza.net>
2010-12-28 19:53:13 -0200

    Added a simple functional test for post on /conferences/new

--- core/tests/functional/test_views.py
+++ core/tests/functional/test_views.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from core.tests import FunctionalCoreTestCase
-from nose.tools import assert_true
+from nose.tools import assert_true, assert_equals
+from core.models import Conference
 
 class FunctionalConferenceViewsSpec(FunctionalCoreTestCase):
 
@@ -7,3 +9,9 @@ class FunctionalConferenceViewsSpec(FunctionalCoreTestCase):
         "New conference url (/conferences/new) should contain the text \"New conference\""
         response = self.client.get('/conferences/new')
         assert_true('New conference' in response.content)
+
+    def should_save_a_conference_by_posting_data_to_new_conference_url(self):
+        title = u'Python Brasil'
+        response = self.client.post('/conferences/new', data={ u'title' : title, u'description' : u'Conferência brasileira de Python' })
+        conference = Conference.objects.filter(title=title).all()[0]
+        assert_equals(conference.title, title)
